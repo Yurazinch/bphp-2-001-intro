9aaee0f3bc00ac78d71efef896eabb53d4637c6c
Yuriy Zinchenko <yurazinch@gmail.com>
2025-06-19 11:15:39 +0300

    исправил index3

--- index3.php
+++ index3.php
@@ -7,19 +7,19 @@ $variable = 'one';
 // $variable = [];
 
 switch (true) {
-    case is_bool($variable) === true:
+    case is_bool($variable):
         $type = 'bool';
         break;
-    case is_float($variable) === true:
+    case is_float($variable):
         $type = 'float';
         break;
-    case is_int($variable) === true:
+    case is_int($variable):
         $type = 'int';
         break;
-    case is_string($variable) === true:
+    case is_string($variable):
         $type = 'string';
         break;
-    case is_null($variable) === true:
+    case is_null($variable):
         $type = 'null';
         break;
     default: $type = 'other';
